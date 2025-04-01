ce37f74f0d6d371077475687748fb0926e70ad4e
CG506A <GROOVERC@my.erau.edu>
2025-03-31 17:49:06 -0700

    Add files via upload

--- legalMove.c
+++ legalMove.c
@@ -3,55 +3,66 @@
 
 #define BOARD 10
 
-int legalMove(int board[BOARD][BOARD], int board2[BOARD][BOARD], int coord[2], int newCoord[2], int turn);
+int legalMove(int i, int j, int newX, int newY, int turn, int board[BOARD][BOARD], /*Are these necessary?*/int board2[BOARD][BOARD], int coord[2], int newCoord[2], );
 {  
-  int piece = 0;
+  int pieceVal;
 
-  int x1 = coord[0];
+  int x1 = coord[0]; //What were these four lines of code from???
   int y1 = coord[1];
   
   int x2 = newCoord[0];
   int y2 = newCoord[1];
   
-  if ()
+  if (newX >= 9 || newY >= 9 || newX <= 0 || newY <= 0)
   {
     printf("This is beyond the constraints of the board. Please try again.\n");
     return 0;
   }
   else
   {
-    piece = board[y1][x1];
+    pieceVal = board[i][j];
   }
   
-  if (((turn % 2 == 0) && (piece > 0)) || ((turn % 2 == 1) && (piece < 0)))
+  /*
+  if(turn % 2 == 0)
   {
-    printf("You can only move your pieces.\n");
-    return 0;
+    type = -1;
   }
+  else
+  {
+    type = 1;
+  }
+  
+  
+  
+  Do we have to  check for whose turn it is or will pieceMove only play when it's the user's turn?
+  */
     
     
-  switch(piece)
+  switch(pieceValue)
   {
     case (1 /*pawn*/):
-      if (newX == /*old input + 1*/ && newY == /*old input + 1*/ && board[newX][newY] < 0)
+      if (newX == (j+1) && (newY == i+1) && board[newX][newY] < 0)
       {
         /*replace enemy piece with pawn*/
+        printf("You captured a piece with your pawn!\n");
         return 1;
       }
-      else if (newX == /*old input - 1*/ && newY == /*old input + 1*/ && board[newX][newY] < 0)
+      else if (newX == (j-1) && newY == (i+1) && board[newX][newY] < 0)
       {
         /*replace enemy piece with pawn*/
+        printf("You captured a piece with your pawn!\n");
         return 1;
       }
-      else if (newX == /*old input*/ && newY == /*old input + 1*/ && board[newX][newY] == 0)
+      else if (newX == j && newY == (i+1) && board[newX][newY] == 0)
       {
+        /*Returns positive check since space in front of pawn is empty*/
         return 1;
-        /*Returns positive check if space in front of pawn in empty*/
       }
-      else if (newX == /*initial x input*/ && /*old y input*/ == 2 && board[newX][newY] == 0 && /*new y input*/ == /*old y input + 2*/)
+      else if (newX == j && i == 2 && board[newX][newY] == 0 && newY == i+2)
       {
-        return 1;
         /*Returns positive check if space in front of pawn in empty and on initial row*/
+        return 1;
       }
       else
       {
@@ -59,23 +70,27 @@ int legalMove(int board[BOARD][BOARD], int board2[BOARD][BOARD], int coord[2], i
       }
    
     case (2 /*knight*/):
-    
+      
     
     
     case (3 /*bishop*/):
-      for (i = 0; i =< 8; i + 2)
+      if (newX == (j+1) && (newY == i+1) && board[newX][newY] < 0)
       {
-        /*run line of sight function to check for piece*/
+        printf("You captured a piece with your pawn!\n");
+        return 1;
       }
-    
-    
-      if ()
+      else if (newX == (j-1) && newY == (i+1) && board[newX][newY] < 0)
+      {
+        printf("You captured a piece with your pawn!\n");
+        return 1;
+      }
+      else if (newX == j && newY == (i+1) && board[newX][newY] == 0)
       {
         return 1;
       }
-      else if ()
+      else if (newX == j && i == 2 && board[newX][newY] == 0 && newY == i+2)
       {
-        return 1;
+      
       }
       else
       {
